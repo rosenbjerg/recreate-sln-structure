42e0f6d6a9b26b880654b8b7a2e06f47323183fd
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2023-02-07 17:10:52 +0100

    Fix naming in example

--- RecreateSolutionStructure/Program.cs
+++ RecreateSolutionStructure/Program.cs
@@ -4,7 +4,7 @@ using RecreateSolutionStructure;
 var solutionFileArgument = new Argument<FileInfo>(name: "path to sln", description: "File path to the solution (.sln) file");
 var rootCommand = new RootCommand("Recreate solution directory tree, i.e. for use in building a containerized .NET application with cached restore layer\n" +
                                   "Takes the path to a solution (.sln) file as input, and moves\n" +
-                                  "Example: 'recreate-solution-directory-tree MySolution.sln'") { Name = "recreate-sln-structure" };
+                                  "Example: 'recreate-sln-structure MySolution.sln'") { Name = "recreate-sln-structure" };
 rootCommand.AddArgument(solutionFileArgument);
 rootCommand.SetHandler(SolutionDirectoryTreeRecreator.Recreate, solutionFileArgument);
 
