5175c8cfb642c5bc67fa81c3b529a819fa383caa
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2023-02-07 16:58:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# A dotnet tool for moving project files into the directories specified by the solutino (.sln) file
+
+Useful for optimizing cache reuse in containerized .NET builds
+
+```
+Description:
+Recreate solution directory tree, i.e. for use in building a containerized .NET application with cached restore layer
+Takes the path to a solution (.sln) file as input, and moves
+Example: 'recreate-solution-directory-tree MySolution.sln'
+
+Usage:
+recreate-solution-directory-tree <path to sln> [options]
+
+Arguments:
+<path to sln>  File path to the solution (.sln) file
+
+Options:
+--version       Show version information
+-?, -h, --help  Show help and usage information
+```
---
+++ RecreateSolutionStructure/SolutionDirectoryTreeRecreator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Build.Construction;
+
+namespace RecreateSolutionStructure;
+
+public class SolutionDirectoryTreeRecreator
+{
+    public static void Recreate(FileInfo solutionFile)
+    {
+        var solution = SolutionFile.Parse(solutionFile.FullName);
+        var solutionDirectory = solutionFile.Directory!.FullName;
+        foreach (var project in solution.ProjectsInOrder)
+        {
+            if (project.ProjectType == SolutionProjectType.SolutionFolder)
+                continue;
+
+            var destinationPath = Path.Combine(solutionDirectory, Path.GetRelativePath(solutionDirectory, Path.GetFullPath(project.AbsolutePath)));
+            var projectName = Path.GetFileName(destinationPath);
+            var currentPath = Path.Combine(solutionDirectory, projectName);
+
+            var relativeDestinationPath = Path.GetRelativePath(".", destinationPath);
+            var relativeCurrentPath = Path.GetRelativePath(".", destinationPath);
+
+            if (File.Exists(destinationPath))
+            {
+                Console.WriteLine($"Skipping - {relativeDestinationPath} already exists");
+            }
+            else if (File.Exists(currentPath))
+            {
+                var directoryTree = Path.GetDirectoryName(destinationPath)!;
+                Directory.CreateDirectory(directoryTree);
+                File.Move(currentPath, destinationPath);
+                Console.WriteLine($"{relativeCurrentPath} moved to {relativeDestinationPath}");
+            }
+            else
+            {
+                Console.Error.WriteLine($"Project file not found in same directory as solution file: {relativeCurrentPath}");
+            }
+        }
+    }
+}
